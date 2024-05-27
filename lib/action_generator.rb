de4482c6b8e51fe0c38f00cbac185b20e66e1174
Joel AZEMAR <joel.azemar@gmail.com>
2024-05-27 12:18:58 +0200

    Add Action Generators

--- lib/action_generator.rb
+++ lib/action_generator.rb
@@ -2,11 +2,10 @@
 
 require "zeitwerk"
 loader = Zeitwerk::Loader.for_gem
+loader.ignore("#{__dir__}/generators")
 loader.setup
-
-
 
 module ActionGenerator
   class Error < StandardError; end
-  
+
 end
--- lib/action_generator/main.rb
+++
@@ -1,7 +0,0 @@
-module ActionGenerator
-  class Main
-    def action_generator(input)
-      input
-    end
-  end
-end
